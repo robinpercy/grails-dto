5a883c3b0c307ad0f072434766ac553676b31569
Robin Percy <rpercy@vivonet.com>
2012-01-22 21:27:07 -0800

    Roughed-in two new pieces of functionality: 1. adding the dto.groovy = true flag to Config.groovy will generate Groovy DTOs in src/groovy 2. transient properties are now included in the generated DTOs
    Still TODO:
    - make the transient properties functionality configurable
    - Refactor changes to be cleaner and reduce duplicated code
    - Add unit tests

---
+++ src/groovy/org/codehaus/groovy/grails/plugins/dto/GroovyGrailsDtoGenerator.groovy
@@ -0,0 +1,11 @@
+package org.codehaus.groovy.grails.plugins.dto
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rpercy
+ * Date: 12-01-22
+ * Time: 3:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class GroovyGrailsDtoGenerator {
+}
